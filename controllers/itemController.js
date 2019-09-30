bb5c4619f033920de65aebcabd5ea7a82aa91c9b
Jake Donahue <jdonahue135@gmail.com>
2019-09-29 21:52:57 -0400

    Implement Item Create

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,4 +1,9 @@
 var Item = require('../models/item');
+var Category = require('../models/category');
+
+
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 //Display list of all Items.
 exports.item_list = function(req, res, next) {
@@ -28,14 +33,65 @@ exports.item_detail = function(req, res, next) {
 };
 
 // Display item create form on GET.
-exports.item_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: item create GET');
+exports.item_create_get = function(req, res, next) {
+    // Display all categories
+    Category.find({}, 'name')
+    .exec(function (err, list_categories) {
+        if (err) { return next(err); }
+        //Successful, so render
+        res.render('item_form', { title: 'Create Club', categories : list_categories });
+    });
 };
 
 // Handle item create on POST.
-exports.item_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: item create POST');
-};
+exports.item_create_post = [
+
+    //Validate fields.
+    body('name').isLength({ min: 1 }).trim().withMessage('Name must be specified.'),
+    body('description').isLength({ min: 1 }).trim().withMessage('Description must be specified.'),
+    body('category').isLength({ min: 1 }).trim().withMessage('Category must be specified'),
+    body('price').isLength({ min: 1 }).trim().withMessage('Price must be specified'),
+    body('number_in_stock').isLength({ min: 1 }).trim().withMessage('Number in stock must be specified'),
+    
+    // Sanitize fields (using wildcard).
+    sanitizeBody('*').escape(),
+
+    //Process request after validation and sanitization.
+    (req, res, next) => {
+
+        //Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            //There are errors. Render form again with sanitized values/errors messages.
+            Category.find({}, 'name')
+                .exec(function (err, list_categories) {
+                    if (err) { return next(err); }
+
+                    res.render('item_form', { title: 'Create Item', categories: list_categories, item: req.body, errors: errors.array() });
+                    return;
+            });
+        }    
+        else {
+            //Data from form is valid.
+
+            //Create an Item object with escaped and trimmed data.
+            var item = new Item(
+                {
+                    name: req.body.name,
+                    description: req.body.description,
+                    category: req.body.category,
+                    price: req.body.price,
+                    number_in_stock: req.body.number_in_stock
+                });
+            item.save(function (err) {
+                if (err) { return next(err); }
+                //Successful - redirect to new Item record.
+                res.redirect(item.url);
+            });
+        }
+    }
+];
 
 // Display item delete form on GET.
 exports.item_delete_get = function(req, res) {
