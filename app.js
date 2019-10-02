b1db203c50ca22744fb895b06fb1563f6e5707cf
jdonahue135 <jdonahue135@gmail.com>
2019-10-02 13:08:22 -0400

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://user:user@cluster0-acdlq.azure.mongodb.net/test?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://user:user@cluster0-acdlq.azure.mongodb.net/test?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
